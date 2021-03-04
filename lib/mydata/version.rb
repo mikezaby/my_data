25c9fa956c1b7cdd9bb563bbc12f2fb6cad6db4a
Michalis Zamparas <mikezaby@gmail.com>
2021-03-04 12:12:23 +0200

    Initialize project

---
+++ lib/mydata/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mydata
+  VERSION = "0.1.0"
+end
---
+++ spec/mydata_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mydata do
+  it "has a version number" do
+    expect(Mydata::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
