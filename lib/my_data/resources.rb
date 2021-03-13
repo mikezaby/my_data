0618abf4f695a598dba3c6f48198edb19a8913a4
Michalis Zamparas <mikezaby@gmail.com>
2021-03-13 15:44:09 +0200

    Rename ResponsesDoc to ResponseDoc to follow documentation

--- lib/my_data/resources.rb
+++ lib/my_data/resources.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module MyData::Resources
-  autoload :ResponsesDoc, "my_data/resources/responses_doc"
+  autoload :ResponseDoc, "my_data/resources/response_doc"
   autoload :ResponseType, "my_data/resources/response_type"
   autoload :ErrorType, "my_data/resources/error_type"
   autoload :Inv, "my_data/resources/inv"
--- lib/my_data/resources/response_doc.rb
+++ lib/my_data/resources/response_doc.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MyData::Resources::ResponsesDoc
+class MyData::Resources::ResponseDoc
   include MyData::Resource
 
   attribute :response, :resource, class_name: "ResponseType", collection: true
