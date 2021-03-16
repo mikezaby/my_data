f74c22b659dfe2ef4a8ddd0d072c176b8444e75c
Michalis Zamparas <mikezaby@gmail.com>
2021-03-16 17:43:26 +0200

    Add reload method for development reasons

--- lib/my_data.rb
+++ lib/my_data.rb
@@ -6,13 +6,18 @@ require "faraday"
 require "nokogiri"
 require "zeitwerk"
 
-loader = Zeitwerk::Loader.for_gem
-loader.setup
-
 module MyData
+  LOADER = Zeitwerk::Loader.for_gem
+  LOADER.enable_reloading
+  LOADER.setup
+
   class Error < StandardError; end
 
   def self.root
     File.expand_path '../..', __FILE__
+  end
+
+  def self.reload!
+    MyData::LOADER.reload
   end
 end
