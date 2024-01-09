ffb96afc18c69935a6f7e5962972eff77e383e75
Michalis Zamparas <mikezaby@gmail.com>
2024-01-09 17:16:27 +0200

    Update xsd to 1.0.7 version

---
+++ lib/my_data/resources/inv/entity_type.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class MyData::Resources::Inv::EntityType
+  include MyData::Resource
+
+  xsd_complex_type
+end
---
+++ lib/my_data/resources/reception_providers_type.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class MyData::Resources::ReceptionProvidersType
+  include MyData::Resource
+
+  xsd_complex_type
+end
